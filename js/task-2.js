73688180f830332d37ab4cd342f0f0d8c93543d2
YuliiaHosteva <140606153+YuliiaHosteva@users.noreply.github.com>
2023-12-08 15:47:19 +0100

    addCss.js

--- js/task-2.js
+++ js/task-2.js
@@ -1,26 +1,37 @@
 const images = [
   {
-    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260",
-    alt: "White and Black Long Fur Cat",
+    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'White and Black Long Fur Cat',
   },
   {
-    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260",
-    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
   },
   {
-    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260",
-    alt: "Group of Horses Running",
+    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Group of Horses Running',
+  },
+  {
+    url: 'https://images.pexels.com/photos/66898/elephant-cub-tsavo-kenya-66898.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    alt: 'Elephant Beside on Baby Elephant',
+  },
+  {
+    url: 'https://images.pexels.com/photos/37833/rainbow-lorikeet-parrots-australia-rainbow-37833.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    alt: 'Blue Geeen and Orange Parrot',
+  },
+  {
+    url: 'https://images.pexels.com/photos/247376/pexels-photo-247376.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    alt: 'Zebras on Zebra',
   },
 ];
 
-// Отримуємо елемент ul.gallery
-const gallery = document.querySelector('.gallery');
+const galleryContainer = document.querySelector('.gallery');
 
 // Створюємо фрагмент для оптимізації додавання елементів в DOM
 const fragment = document.createDocumentFragment();
 
 // Проходимося по масиву images та створюємо для кожного об'єкта елемент li з дочірнім елементом img
-images.forEach(image => {
+images.forEach((image) => {
   const listItem = document.createElement('li');
   const imageElement = document.createElement('img');
 
@@ -36,4 +47,7 @@ images.forEach(image => {
 });
 
 // Додаємо фрагмент (весь список) в ul.gallery за одну операцію
-gallery.appendChild(fragment);
+galleryContainer.appendChild(fragment);
+
+// Додаємо класи для оформлення за допомогою CSS
+galleryContainer.classList.add('gallery-container');
